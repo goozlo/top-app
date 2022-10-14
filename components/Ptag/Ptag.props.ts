5b3b58eebfc65ce63e6e7dc820aebf82b824ecd7
Nikita <nikitaleonov00@mail.ru>
2022-10-14 16:29:37 +0300

    added statefull components

---
+++ components/Ptag/Ptag.props.ts
@@ -0,0 +1,6 @@
+import {DetailedHTMLProps, HTMLAttributes, ReactNode} from "react";
+
+export interface PtagProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
+    fontSize?: '14px' | '16px' | '18px'
+    children: ReactNode
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import {Button, Htag} from "../components";
+import React from "react";
+import {Button, Htag, Ptag, Rating, Tag} from "../components";
 
 export default function Home(): JSX.Element {
+
+    const [rating, setRating] = React.useState<number>(4);
+
     return (
         <div>
-            <Htag tag='h3'>Тайтл</Htag>
-            <Button appearance='primary'>Кнопка</Button>
-            <Button appearance='ghost'>Кнопка</Button>
+            <Htag tag='h3'>Hello world</Htag>
+            <Button appearance='primary' arrow='right'>Кнопка</Button>
+            <Button appearance='ghost' arrow='down'>Кнопка</Button>
+            <Ptag fontSize='14px'>hello world</Ptag>
+            <Ptag fontSize='16px'>hello world</Ptag>
+            <Ptag fontSize='18px'>hello world</Ptag>
+            <Tag color='primary' size='large'>Hello World</Tag>
+            <Tag color='primary'>Hello World</Tag>
+            <Tag color='red' size='large'>Hello World</Tag>
+            <Rating rating={rating} setRating={setRating} isEditable/>
         </div>
     );
 }

6402a9768b361a599494a0829efb46dd6412030c
Nikita <nikitaleonov00@mail.ru>
2022-10-22 18:29:46 +0300

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import axios from 'axios';
 import {Button, Htag, Ptag, Rating, Tag} from "../components";
 import {withLayout} from "../layout/Layout";
+import {GetStaticProps} from "next";
+import {IMenuItem} from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({menu}: HomeProps): JSX.Element {
     const [rating, setRating] = React.useState<number>(4);
 
     return (
@@ -17,8 +20,27 @@ function Home(): JSX.Element {
             <Tag color='primary'>Hello World</Tag>
             <Tag color='red' size='large'>Hello World</Tag>
             <Rating rating={rating} setRating={setRating} isEditable/>
+            {menu.map(i => i)}
         </>
     );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+    const firstCategory = 0;
+    const {data: menu} = await axios.post<IMenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory
+    });
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    menu: IMenuItem[]
+    firstCategory: number
+}
